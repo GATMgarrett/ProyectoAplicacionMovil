bb9f060ce935b9deb7c16b96b0000e3d1a78c00e
GATM <german.gt285@gmail.com>
2019-06-15 21:41:00 -0400

    Hola

--- app/src/main/java/com/univalle/proyectogd/MainActivity.java
+++ app/src/main/java/com/univalle/proyectogd/MainActivity.java
@@ -1,13 +1,27 @@
 package com.univalle.proyectogd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSalir = findViewById(R.id.btnCerrarAplicacion);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent salida = new Intent( Intent.ACTION_MAIN); //Llamando a la activity principal
+                finish(); // La cerramos.
+            }
+        });
     }
 }
